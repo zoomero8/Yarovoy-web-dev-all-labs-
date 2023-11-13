ed9f13f9f7d6b57212298fb0f04fd7794507045a
zoomero8 <d.r.iarovoi@stud.mospolytech.ru>
2023-11-13 21:46:56 +0300

    ajaz

--- lab8/static/main.js
+++ lab8/static/main.js
@@ -1,3 +1,4 @@
+//Создание кнопки пагинации
 function createPageBtn(page, classes=[]) {
     let btn = document.createElement('button');
     classes.push('btn');
@@ -9,6 +10,7 @@ function createPageBtn(page, classes=[]) {
     return btn;
 }
 
+//Отображение кнопок пагинации
 function renderPaginationElement(info) {
     let btn;
     let paginationContainer = document.querySelector('.pagination');
@@ -16,7 +18,7 @@ function renderPaginationElement(info) {
 
     btn = createPageBtn(1, ['first-page-btn']);
     btn.innerHTML = 'Первая страница';
-    if (info.current_page == 1) {
+    if (info.current_page == 1 || info.total_count==0) {
         btn.style.visibility = 'hidden';
     }
     paginationContainer.append(btn);
@@ -33,16 +35,18 @@ function renderPaginationElement(info) {
 
     btn = createPageBtn(info.total_pages, ['last-page-btn']);
     btn.innerHTML = 'Последняя страница';
-    if (info.current_page == info.total_pages) {
+    if (info.current_page == info.total_pages || info.total_count==0) {
         btn.style.visibility = 'hidden';
     }
     paginationContainer.append(btn);
 }
 
+//Обработчик событый для кнопки, изменяющей кол-во записей на странице
 function perPageBtnHandler(event) {
     downloadData(1);
 }
 
+//Отображение данных с текущей страницы
 function setPaginationInfo(info) {
     document.querySelector('.total-count').innerHTML = info.total_count;
     let start = info.total_count > 0 ? (info.current_page - 1)*info.per_page + 1 : 0;
@@ -51,6 +55,7 @@ function setPaginationInfo(info) {
     document.querySelector('.current-interval-end').innerHTML = end;
 }
 
+//Обработчик событый для кнопок
 function pageBtnHandler(event) {
     if (event.target.dataset.page) {
         downloadData(event.target.dataset.page);
@@ -58,6 +63,7 @@ function pageBtnHandler(event) {
     }
 }
 
+//Создание блока с именем автора факта
 function createAuthorElement(record) {
     let user = record.user || {'name': {'first': '', 'last': ''}};
     let authorElement = document.createElement('div');
@@ -66,6 +72,7 @@ function createAuthorElement(record) {
     return authorElement;
 }
 
+//Создание блока с кол-вом лайков
 function createUpvotesElement(record) {
     let upvotesElement = document.createElement('div');
     upvotesElement.classList.add('upvotes');
@@ -73,6 +80,7 @@ function createUpvotesElement(record) {
     return upvotesElement;
 }
 
+//Создание блока для футера с автором факта и кол-вом лайков
 function createFooterElement(record) {
     let footerElement = document.createElement('div');
     footerElement.classList.add('item-footer');
@@ -81,6 +89,7 @@ function createFooterElement(record) {
     return footerElement;
 }
 
+//Создание блока для контента
 function createContentElement(record) {
     let contentElement = document.createElement('div');
     contentElement.classList.add('item-content');
@@ -88,6 +97,7 @@ function createContentElement(record) {
     return contentElement;
 }
 
+//Создание элементов
 function createListItemElement(record) {
     let itemElement = document.createElement('div');
     itemElement.classList.add('facts-list-item');
@@ -96,6 +106,7 @@ function createListItemElement(record) {
     return itemElement;
 }
 
+//Добавление данных из API на страницу
 function renderRecords(records) {
     let factsList = document.querySelector('.facts-list');
     factsList.innerHTML = '';
@@ -104,37 +115,98 @@ function renderRecords(records) {
     }
 }
 
-function downloadData(url) {
+//Загрузка данных из API
+function downloadData(page=1) {
+    let searchField = document.querySelector('.search-field').value;
+
+    let factsList = document.querySelector('.facts-list');
+    let url = new URL(factsList.dataset.url);
+
+    let perPage = document.querySelector('.per-page-btn').value;
+
+    url.searchParams.append('page', page);
+    url.searchParams.append('per-page', perPage);
+    //задание 1
+    url.searchParams.append('q', searchField);
+
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.responseType = 'json';
+
     xhr.onload = function () {
         renderRecords(this.response.records);
         setPaginationInfo(this.response['_pagination']);
         renderPaginationElement(this.response['_pagination']);
+    }
+    xhr.send();
+}
+
+//Задание 2: Функция для очистки выпадающего списка
+function clearAutocomplete() {
+    let autocompleteDropdown = document.querySelector('.autocomplete-dropdown');
+    autocompleteDropdown.innerHTML = '';
+    autocompleteDropdown.style.display = 'none';
+}
+
+//Задание 2: Функция для отображения результатов в выпадающем списке
+function displayAutocomplete(results) {
+    let autocompleteDropdown = document.querySelector('.autocomplete-dropdown');
+
+    for (let i = 0; i < results.length; i++) {
+        let option = document.createElement('div');
+        option.classList.add('autocomplete-option');
+        option.textContent = results[i];
+        option.addEventListener('click', function () {
+            // При клике подставляем выбранный вариант в поле поиска
+            document.querySelector('.search-field').value = results[i];
+            clearAutocomplete();
+            downloadData();
+        });
+        autocompleteDropdown.appendChild(option);
+    }
+
+    autocompleteDropdown.style.display = 'block';
+}
+
+
+// Задание 2: Функция для обработки запроса автодополнения
+function autocompleteRequest() {
+    let searchField = document.querySelector('.search-field').value;
+
+    let autocompleteUrl = 'http://cat-facts-api.std-900.ist.mospolytech.ru/autocomplete?q=' + searchField;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', autocompleteUrl);
+    
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+        if (this.response) {
+            // Очищаем предыдущие результаты
+            clearAutocomplete();
+
+            // Отображаем результаты в выпадающем списке
+            displayAutocomplete(this.response);
+        }
     };
     xhr.send();
 }
-function searchRecords() {
-    let searchInput = document.querySelector('.search-field');
-    let searchQuery = searchInput.value.trim();
 
-    let factsList = document.querySelector('.facts-list');
-    let url = new URL(factsList.dataset.url);
-    let perPage = document.querySelector('.per-page-btn').value;
 
-    url.searchParams.set('page', 1);
-    url.searchParams.set('per-page', perPage);
-    url.searchParams.set('q', searchQuery);
 
-    downloadData(url);
+//задание 1
+function SearchdownloadData() {
+    downloadData();
+
+    clearAutocomplete();
 }
 
 window.onload = function () {
-    downloadData(); // начальная загрузка данных
-
+    downloadData();
     document.querySelector('.pagination').onclick = pageBtnHandler;
     document.querySelector('.per-page-btn').onchange = perPageBtnHandler;
-
-    document.querySelector('.search-btn').addEventListener('click', searchRecords); 
+    //задание 1    
+    document.querySelector('.search-btn').onclick = SearchdownloadData;
+    //задание 2
+    document.querySelector('.search-field').oninput = autocompleteRequest;
 }
