200b9673a4cf233d404aeee0d1868b4a7d44bf33
zoomero8 <d.r.iarovoi@stud.mospolytech.ru>
2023-11-22 14:38:08 +0300

    -09

--- lab9/index.php
+++ lab9/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Лабораторная работа №9</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Libre+Franklin&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
 
@@ -24,16 +24,22 @@
 
     <main class="main">
         <form method="post" action="" class="contact-form">
-            <label class="label" for="startArgument">Начальный аргумент:</label>
-            <input type="number" name="startArgument" id="startArgument" class="form-field" required>
-            <br>
-            <label for="numValues">Количество значений функции:</label>
-            <input type="number" name="numValues" id="numValues" class="form-field" required>
-            <br>
-            <label for="step">Шаг:</label>
-            <input type="number" name="step" id="step" class="form-field" required>
-            <br>
-            <label for="layoutType">Тип верстки:</label>
+            <?php
+            function generateInput($name, $label, $type = "number", $required = true)
+            {
+                echo "<label for=\"$name\" class=\"label\">$label:</label>";
+                echo "<input type=\"$type\" name=\"$name\" id=\"$name\" class=\"form-field\" required=\"$required\">";
+                echo "<br>";
+            }
+
+            generateInput("startArgument", "Начальный аргумент");
+            generateInput("numValues", "Количество значений функции");
+            generateInput("step", "Шаг");
+            generateInput("min_value", "Минимальное значение");
+            generateInput("max_value", "Максимальное значение");
+            ?>
+
+            <label for="layoutType" class="label">Тип верстки:</label>
             <select name="layoutType" id="layoutType" class="form-field">
                 <option value="A">Тип A</option>
                 <option value="B">Тип B</option>
@@ -47,88 +53,234 @@
 
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // валидация и выгрузка значений из формы
             $startArgument = isset($_POST["startArgument"]) && is_numeric($_POST["startArgument"]) ? (int)$_POST["startArgument"] : 0;
+            $min_value = isset($_POST["min_value"]) && is_numeric($_POST["min_value"]) ? (int)$_POST["min_value"] : PHP_INT_MIN;
+            $max_value = isset($_POST["max_value"]) && is_numeric($_POST["max_value"]) ? (int)$_POST["max_value"] : PHP_INT_MAX;
             $numValues = isset($_POST["numValues"]) && is_numeric($_POST["numValues"]) ? (int)$_POST["numValues"] : 0;
             $step = isset($_POST["step"]) && is_numeric($_POST["step"]) ? (int)$_POST["step"] : 0;
             $layoutType = isset($_POST["layoutType"]) ? $_POST["layoutType"] : '';
             $values = [];
+            $min = 0;
+            $max = 0;
+            $sum = 0;
+
+            function calculateFunction($argument)
+            {
+                if ($argument <= 10) {
+                    return (3 * pow($argument, 2) + 2);
+                } elseif ($argument > 10 && $argument < 20) {
+                    return (5 * $argument + 7);
+                } else {
+                    if ($argument == 22) {
+                        return 'error';
+                    } else {
+                        return ($argument / (22 - $argument));
+                    }
+                }
+            }
+
+            function cycleWithCounter($x, $numValues, $step, $type, $min_value, $max_value, $sum, $min, $max)
+            {
+                //цикл со счетчиком
+                for ($i = 0; $i < $numValues; $i++, $x += $step) {
+                    $k = $i + 1;
+
+                    $value = calculateFunction($x);
+
+                    if ($value != 'error') {
+                        $sum += $value;
+                        $min = min($min, $value);
+                        $max = max($max, $value);
+                    }
+
+                    switch ($type) {
+                        case 'A':
+                            echo "f($x) = $value<br><br>";
+                            break;
+                        case 'B':
+                            echo "<ul><li>f($x) = $value</li></ul>";
+                            break;
+                        case 'C':
+                            echo "<ol start='$k'><li>f($x) = $value</li></ol>";
+                            break;
+                        case 'D':
+                            echo "<table border='1' cellspacing='0' cellpadding='10'>
+                                    <tr>
+                                        <td>Шаг</td>
+                                        <td>Аргумент функции</td>
+                                        <td>Значение функции</td>
+                                    </tr>
+                                    <tr>
+                                        <td>$i</td>
+                                        <td>$x</td>
+                                        <td>$value</td>
+                                    </tr>
+                                </table>";
+                            break;
+                        case 'E':
+                            echo '<div style="border: 2px solid red; display: inline-block; margin-right: 8px;">';
+                            echo "f($x) = $value";
+                            echo '</div>';
+                            break;
+                        default:
+                            echo "f($x) = $value<br><br>";
+                    }
+
+                    if ($value >= $max_value || $value < $min_value)
+                        break;
+                }
+
+                return "<br><br>";
+            }
+
+            function cycleWithPrecondition($x, $numValues, $step, $type, $min_value, $max_value, $sum, $min, $max)
+            {
+                //цикл с предусловием
+                $i = 0;
+                while ($i < $numValues && ($x <= $max_value && $x >= $min_value)) {
+                    $k = $i + 1;
+                    $argument = $x;
+                    $value = calculateFunction($argument);
+
+                    if ($value != 'error') {
+                        $sum += $value;
+                        $min = min($min, $value);
+                        $max = max($max, $value);
+                    }
+
+                    switch ($type) {
+                        case 'A':
+                            echo "f($x) = $value<br><br>";
+                            break;
+                        case 'B':
+                            echo "<ul><li>f($x) = $value</li></ul>";
+                            break;
+                        case 'C':
+                            echo "<ol start='$k'><li>f($x) = $value</li></ol>";
+                            break;
+                        case 'D':
+                            echo "<table border='1' cellspacing='0' cellpadding='10'>
+                                    <tr>
+                                        <td>Шаг</td>
+                                        <td>Аргумент функции</td>
+                                        <td>Значение функции</td>
+                                    </tr>
+                                    <tr>
+                                        <td>$i</td>
+                                        <td>$x</td>
+                                        <td>$value</td>
+                                    </tr>
+                                </table>";
+                            break;
+                        case 'E':
+                            echo '<div style="border: 2px solid red; display: inline-block; margin-right: 8px;">';
+                            echo "f($x) = $value";
+                            echo '</div>';
+                            break;
+                        default:
+                            echo "f($x) = $value<br><br>";
+                    }
+
+                    $i++;
+                    $x += $step;
+                }
+
+                return "<br><br>";
+            }
+
+            function cycleWithPostcondition($x, $numValues, $step, $type, $min_value, $max_value, $sum, $min, $max)
+            {
+                //цикл с постусловием
+                $i = 0;
+                do {
+                    $k = $i + 1;
+                    $argument = $x;
+
+                    $value = calculateFunction($argument);
+
+                    if ($value != 'error') {
+                        $sum += $value;
+                        $min = min($min, $value);
+                        $max = max($max, $value);
+                    }
+
+                    switch ($type) {
+                        case 'A':
+                            echo "f($x) = $value<br><br>";
+                            break;
+                        case 'B':
+                            echo "<ul><li>f($x) = $value</li></ul>";
+                            break;
+                        case 'C':
+                            echo "<ol start='$k'><li>f($x) = $value</li></ol>";
+                            break;
+                        case 'D':
+                            echo "<table border='1' cellspacing='0' cellpadding='10'>
+                                    <tr>
+                                        <td>Шаг</td>
+                                        <td>Аргумент функции</td>
+                                        <td>Значение функции</td>
+                                    </tr>
+                                    <tr>
+                                        <td>$i</td>
+                                        <td>$x</td>
+                                        <td>$value</td>
+                                    </tr>
+                                </table>";
+                            break;
+                        case 'E':
+                            echo '<div style="border: 2px solid red; display: inline-block; margin-right: 8px;">';
+                            echo "f($x) = $value";
+                            echo '</div>';
+                            break;
+                        default:
+                            echo "f($x) = $value<br><br>";
+                    }
+
+                    $i++;
+                    $x += $step;
+                } while ($i < $numValues && ($x <= $max_value && $x >= $min_value));
+
+                return "<br><br>";
+            }
+
+            $sum = 0;
+            $min = PHP_INT_MAX;
+            $max = PHP_INT_MIN;
 
             for ($i = 0; $i < $numValues; $i++) {
-                $k = $i + 1;
                 $argument = $startArgument + ($i * $step);
 
+                $value = calculateFunction($argument);
 
-                // Проверка $startArgument перед использованием в формулах
-                if ($startArgument == 22) {
-                    $value = 'Ошибка, не удовлетворяет ОДЗ';
+                if ($value != 'error' && $value >= $min_value && $value <= $max_value) {
+                    $sum += $value;
+                    $min = min($min, $value);
+                    $max = max($max, $value);
                 } else {
-                    $value = ($argument <= 10) ? (3 * pow($startArgument, 2) + 2) : (($argument > 10 && $argument < 20) ? (5 * $argument + 7) : ($argument / (22 - $startArgument)));
-                }
-
-                $values[] = $value;
-
-                // Вывод в зависимости от типа верстки
-                if ($layoutType == 'A' || $layoutType == 'B' || $layoutType == 'C' || $layoutType == 'D') {
-                    if ($layoutType == 'A') {
-                        echo "f($argument) = $value<br><br>";
-                    } elseif ($layoutType == 'B') {
-                        echo "<ul><li>f($argument) = $value</li></ul>";
-                    } elseif ($layoutType == 'C') {
-                        echo "<ol start='$k'><li>f($argument) = $value</li></ol>";
-                    } elseif ($layoutType == 'D') {
-                        // Вывод заголовка таблицы
-                        echo "<table border='1' cellspacing='0' cellpadding='10'>
-                        <tr>
-                            <td>№</td>
-                            <td>Аргумент</td>
-                            <td>Значение функции</td>
-                        </tr>";
-                           // Вывод строки таблицы
-                           echo "<tr>
-                           <td>$i</td>
-                           <td>$argument</td>
-                           <td>$value</td>
-                       </tr>";
-                        // Закрытие таблицы
-                        echo "</table>";
-                    }
-                    
-                } elseif ($layoutType == 'E') {
-                    echo "<div class='typeE'>   
-                            f($argument) = $value
-                        </div>";
+                    echo 'Не удовлетворяет отрезку минимума и максимума';
+                    break;
                 }
             }
 
-            // Вычисление дополнительных статистических данных
-            if (!empty($values)) {
-                $maxim = max($values);
-                $minim = min($values);
-                $avg = count($values) > 0 ? array_sum($values) / count($values) : 0;
-                $summa = array_sum($values);
+            "<br><br>";
+            echo cycleWithCounter($startArgument, $numValues, $step, 'counter', $min_value, $max_value, $sum, $min, $max);
+            echo cycleWithPrecondition($startArgument, $numValues, $step, 'counter', $min_value, $max_value, $sum, $min, $max);
+            echo cycleWithPostcondition($startArgument, $numValues, $step, 'counter', $min_value, $max_value, $sum, $min, $max);
 
-            echo "<br>Максимальное значение: $maxim<br><br>";
-            echo "Минимальное значение: $minim<br><br>";
-            echo "Среднее арифметическое: $avg<br><br>";
-            echo "Сумма значений: $summa<br>";
-            } else {
-                // обработка случая, когда $values пуст
-                $maxim = 'Массив значений пуст';
-            }
         }
         ?>
     </main>
 
     <footer class="footer">
         <div class="container">
-        <?php
-            if (!empty($layoutType)){
-            echo "<footer><p>Тип верстки: $layoutType</p></footer>";
-            } else{
+            <?php
+            if (!empty($layoutType)) {
+                echo "<footer><p>Тип верстки: $layoutType</p></footer>";
+            } else {
                 $layoutType = 'Верстка еще не выбрана.';
             }
-            ?>   
+            ?>
             2023 Циклические алгоритмы. Условия в алгоритмах. Табулирование функций
         </div>
     </footer>
